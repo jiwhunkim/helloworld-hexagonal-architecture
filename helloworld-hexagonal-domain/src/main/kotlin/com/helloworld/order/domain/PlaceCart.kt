5adc5d2fa3bb9be0ec9325b53e8c59cd4e42ea23
Jiwhun Kim <jiwhun.kim@outlook.com>
2022-05-01 18:12:59 +0900

    장바구니 id 생성 기능 추가

--- helloworld-hexagonal-domain/src/main/kotlin/com/helloworld/order/domain/PlaceCart.kt
+++ helloworld-hexagonal-domain/src/main/kotlin/com/helloworld/order/domain/PlaceCart.kt
@@ -1,10 +1,19 @@
 package com.helloworld.order.domain
 
+import java.time.ZonedDateTime
+import java.time.format.DateTimeFormatter
+
 class PlaceCart(
     val memberNo: Long,
-    val items: List<CartLineItem>
+    val items: List<PlaceCartLineItem>
 ) {
     init {
         require(items.isNotEmpty()) { "require items" }
+    }
+
+    private val formatter: DateTimeFormatter = DateTimeFormatter.ofPattern("yyyyMMddHHmmssSSSSSS")
+
+    fun createId(): String {
+        return "${ZonedDateTime.now().format(formatter)}|${memberNo}"
     }
 }
--- helloworld-hexagonal-domain/src/test/kotlin/com/helloworld/order/domain/PlaceCartSpec.kt
+++ helloworld-hexagonal-domain/src/test/kotlin/com/helloworld/order/domain/PlaceCartSpec.kt
@@ -2,14 +2,29 @@ package com.helloworld.order.domain
 
 import io.kotest.assertions.throwables.shouldThrowExactly
 import io.kotest.core.spec.style.DescribeSpec
+import io.kotest.matchers.nulls.shouldNotBeNull
+import java.math.BigDecimal
 
 class PlaceCartSpec: DescribeSpec() {
     init {
-        describe("constructor") {
+        describe(".constructor") {
             it("장바구니 생성시 최소 하나이상의 상품이 있어야 한다.") {
                 shouldThrowExactly<IllegalArgumentException> {
                     PlaceCart(memberNo = 1L, items = emptyList())
                 }
+            }
+        }
+
+        describe(".createId") {
+            it("현재 시간 nanosecond 와 memberNo 로 cartId 를 생성한다.") {
+                val placeCartLineItem = PlaceCartLineItem(
+                    productId = 1L,
+                    productName = "productName",
+                    quantity = 1,
+                    price = BigDecimal.TEN
+                )
+                val id = PlaceCart(memberNo = 1L, items = listOf(placeCartLineItem)).createId()
+                id.shouldNotBeNull()
             }
         }
     }
